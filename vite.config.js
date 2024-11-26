4e98520c5568d1d0a0bf0b8c6f3db1179fe631d7
EdenKwon <ybinn@caresquare.kr>
2024-11-26 19:32:50 +0900

    FIX : vite.config 에러 fixed

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,7 @@
-<<<<<<< HEAD
 import { fileURLToPath, URL } from 'node:url' 
-=======
-// import { fileURLToPath, URL } from 'node:url'
-//
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-//
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     },
-//   },
-// })
-import { fileURLToPath, URL } from 'node:url'
->>>>>>> c21df164b245dca16065d28741f41650ea0743f8
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-<<<<<<< HEAD
 
-=======
->>>>>>> main
 export default defineConfig({
   plugins: [
     vue(),
@@ -42,8 +19,6 @@ export default defineConfig({
       }
     }
   }
-<<<<<<< HEAD
+
 })
-=======
-})
->>>>>>> main
+
