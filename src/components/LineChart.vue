49f44b5916821981a78b564395f328a3dd35ef3e
yejinCodes <yehi97@gmail.com>
2024-11-29 10:56:19 +0900

    FIX: filler problem in dashboard solved

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -5,44 +5,71 @@
 </template>
 
 <script>
-import { Chart, LineController, LineElement, PointElement, LinearScale, Title, Tooltip, CategoryScale, Legend} from 'chart.js';
-// import Chart from 'chart.js/auto';
-
-Chart.register(LineController, LineElement, PointElement, LinearScale, Title, Tooltip, CategoryScale, Legend);
+import { Chart, LineController, LineElement, PointElement, LinearScale, Title, Tooltip, CategoryScale, Legend } from 'chart.js';
 
 export default {
   props: ['data'],
   mounted() {
-    new Chart(this.$refs.lineChartCanvas, {
-      type: 'line',
-      data: this.data,
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          tooltip: {
-            enabled: true,
-            mode: 'index',
-            intersect: false,
-            position: 'nearest', 
-            yAlign: 'top', 
+    this.renderChart();
+  },
+  watch: {
+    data() {
+      this.renderChart();
+    },
+  },
+  methods: {
+    renderChart() {
+      if (this.chart) {
+        this.chart.destroy();
+      }
+
+      const ctx = this.$refs.lineChartCanvas.getContext('2d');
+
+      const plugins = [LineController, LineElement, PointElement, LinearScale, Title, Tooltip, CategoryScale, Legend];
+      if (this.shouldRegisterFiller()) {
+        import('chart.js/auto').then((chartModule) => {
+          const Filler = chartModule.Filler;
+          plugins.push(Filler);
+          this.createChart(ctx, plugins);
+        });
+      } else {
+        this.createChart(ctx, plugins);
+      }
+    },
+    createChart(ctx, plugins) {
+      Chart.register(...plugins);
+      this.chart = new Chart(ctx, {
+        type: 'line',
+        data: this.data,
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          plugins: {
+            tooltip: {
+              enabled: true,
+              mode: 'index',
+              intersect: false,
+              position: 'nearest',
+              yAlign: 'top',
+            },
+          },
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+            x: {
+              display: true,
+              title: {
+                display: true,
+              },
+            },
           },
         },
-        scales: {                // 스케일 옵션 추가
-          y: {
-            beginAtZero: true,   // y축을 0부터 시작
-          },
-          x: {
-            display: true,
-            title: {
-              display: true,
-            }
-          }
-        }
-/////
-
-      },
-    });
+      });
+    },
+    shouldRegisterFiller() {
+      return this.data && this.data.datasets.some((dataset) => dataset.fill);
+    },
   },
 };
 </script>
